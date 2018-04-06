c97eee4fb0b1d6339d804f05aeb176d568c72fb0
zfreedman <zfreedman92@gmail.com>
2018-04-06 15:35:21 -0700

    feat: Began setting up basic project structure
    Began setting up basic project structure. Recreated files/folders from
    scratch.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+// import
+import React from "react";
+import ReactDOM from "react-dom";
 
-import App from './components/app';
-import reducers from './reducers';
+// api key
+const API_KEY = "AIzaSyCGtIV4hFOR2jXiuJoCWtRk-PmjA_jFnSE";
+/*
+const API_KEY = ((f) => {
+  var raw_file = new XMLHttpRequest();
+  raw_file.open("GET", f, false);
+  raw_file.onreadystatechange = function() {
+    if(raw_file.readyState === 4)
+    {
+      if(raw_file.status === 200 || raw_file.status == 0)
+      {
+        alert(raw_file.responseText);
+        return raw_file.responseText;
+      }
+    }
+  }
+})("./api.txt")
+*/
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// create new copmonenet to produce html
+const App = () => {
+  return <div>Hi!</div>;
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// put component in dom
+ReactDOM.render(<App />, document.querySelector(".container"));
