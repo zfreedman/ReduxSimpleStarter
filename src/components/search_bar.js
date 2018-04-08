6d66875c5cb42afc8d9213410a080c6bf2211d64
zfreedman <zfreedman92@gmail.com>
2018-04-08 11:07:45 -0700

    feat: Began fleshing out other custom components.
    Began fleshing out other custom components. Rendering basic information
    for stubbed surfboard data per each of the 5 videos returned.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -22,9 +22,12 @@ class SearchBar extends Component {
     //return <input onChange={this.onInputChange} />
     return (
       <div>
-        <input onChange={
-          (event) => this.setState({ term: event.target.value })
-        } />
+        <input
+          value={this.state.term}
+          onChange={
+            (event) => this.setState({ term: event.target.value })
+          }
+        />
         Value of the input: {this.state.term}
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,34 +1,42 @@
 // import
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
+// youtube search
+import YTSearch from "youtube-api-search";
+
+// custom components
 import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
+
 // api key
 const API_KEY = "AIzaSyCGtIV4hFOR2jXiuJoCWtRk-PmjA_jFnSE";
-/*
-const API_KEY = ((f) => {
-  var raw_file = new XMLHttpRequest();
-  raw_file.open("GET", f, false);
-  raw_file.onreadystatechange = function() {
-    if(raw_file.readyState === 4)
-    {
-      if(raw_file.status === 200 || raw_file.status == 0)
-      {
-        alert(raw_file.responseText);
-        return raw_file.responseText;
-      }
-    }
-  }
-})("./api.txt")
-*/
 
 // create new copmonenet to produce html
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  // constructor
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    // make search for surf boards
+    YTSearch({key: API_KEY, term: "surfboards"}, (videos) => {
+      // this.setState({ videos: videos });
+      this.setState({ videos }) //equivalent in ES6 (names have to match)
+    });
+  }
+
+  // rendering
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        // passing data from the parent state to the child state (passing props)
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 // put component in dom
